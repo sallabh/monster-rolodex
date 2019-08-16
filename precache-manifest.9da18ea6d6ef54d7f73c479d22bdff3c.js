83692200bba4580ba7d39fac865bae2ba734a06e
sallabh <sallabhk@gmail.com>
2019-08-16 22:36:58 +0000

    Updates

---
+++ precache-manifest.9da18ea6d6ef54d7f73c479d22bdff3c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba7cbb6a4c7f4c063000891e3760ea5b",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "2bdb52118bcc46e5d2b8",
+    "url": "/monster-rolodex/static/css/main.d4c4d115.chunk.css"
+  },
+  {
+    "revision": "32061f2f4fe1438d5314",
+    "url": "/monster-rolodex/static/js/2.67be95f2.chunk.js"
+  },
+  {
+    "revision": "2bdb52118bcc46e5d2b8",
+    "url": "/monster-rolodex/static/js/main.e6078771.chunk.js"
+  },
+  {
+    "revision": "58aab954714b377e1cb3",
+    "url": "/monster-rolodex/static/js/runtime~main.62800703.js"
+  }
+]);
